ad5ca8314031ac5dbfad750023e4420b04ed2f1e
Josh <joshua.quinn.norfolk@gmail.com>
2023-03-10 12:24:19 -0500

    Final commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import scipy.stats
 import streamlit as st
 import time
 
-# these are stateful variables which are preserved as Streamlin reruns this script
 if 'experiment_no' not in st.session_state:
     st.session_state['experiment_no'] = 0
 
@@ -15,15 +14,14 @@ st.header('Tossing a Coin')
 chart = st.line_chart([0.5])
 
 def toss_coin(n):
-
+    
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
-
     mean = None
     outcome_no = 0
     outcome_1_count = 0
 
     for r in trial_outcomes:
-        outcome_no +=1
+        outcome_no += 1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
@@ -32,11 +30,12 @@ def toss_coin(n):
 
     return mean
 
+
 number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
 start_button = st.button('Run')
 
 if start_button:
-    st.write(f'Running the experient of {number_of_trials} trials.')
+    st.write(f'Running the experiment of {number_of_trials} trials.')
     st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
     st.session_state['df_experiment_results'] = pd.concat([
@@ -50,4 +49,4 @@ if start_button:
     st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
 
 st.write(st.session_state['df_experiment_results'])
-
+ 
